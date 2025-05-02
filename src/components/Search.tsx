7099342c61a2792cf9c0bb9151be99ddfc60f1ab
Mikołaj Kondela <mkondela.formal@gmail.com>
2025-05-02 12:02:44 +0200

    Fix search button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,104 +1,116 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useState, forwardRef, useImperativeHandle } from "react";
 import { useSearchParams, useRouter, usePathname } from "next/navigation";
 import Combobox from "@/components/Combobox";
 
-const Search = ({
-  isInstant,
-  onSearch,
-}: {
-  isInstant: boolean;
-  onSearch: (query: string, subject: string, university: string) => void;
-}) => {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [subject, setSubject] = useState("");
-  const [university, setUniversity] = useState("");
+const Search = forwardRef(
+  (
+    {
+      isInstant,
+      onSearch,
+    }: {
+      isInstant: boolean;
+      onSearch: (query: string, subject: string, university: string) => void;
+    },
+    ref
+  ) => {
+    const [searchQuery, setSearchQuery] = useState("");
+    const [subject, setSubject] = useState("");
+    const [university, setUniversity] = useState("");
 
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
 
-  const setSelectedQuery = (value: string) => {
-    setSearchQuery(value);
-    if (isInstant) handleInstantSearch(value, subject, university);
-  };
+    const setSelectedQuery = (value: string) => {
+      setSearchQuery(value);
+      if (isInstant) handleInstantSearch(value, subject, university);
+    };
 
-  const handleInstantSearch = (query: string, subject: string, university: string) => {
-    const params = new URLSearchParams(searchParams?.toString());
-    if (query) {
-      params.set("query", query.toLowerCase());
-    } else {
-      params.delete("query");
-    }
-    if (subject) {
-      params.set("subject", subject.toLowerCase());
-    } else {
-      params.delete("subject");
-    }
-    if (university) {
-      params.set("university", university.toLowerCase());
-    } else {
-      params.delete("university");
-    }
-    replace(`${pathname}?${params.toString()}`);
-  };
+    const handleInstantSearch = (query: string, subject: string, university: string) => {
+      const params = new URLSearchParams(searchParams?.toString());
+      if (query) {
+        params.set("query", query.toLowerCase());
+      } else {
+        params.delete("query");
+      }
+      if (subject) {
+        params.set("subject", subject.toLowerCase());
+      } else {
+        params.delete("subject");
+      }
+      if (university) {
+        params.set("university", university.toLowerCase());
+      } else {
+        params.delete("university");
+      }
+      replace(`${pathname}?${params.toString()}`);
+    };
 
-  const handleSearch = () => {
-    const params = new URLSearchParams();
-    if (searchQuery.trim()) {
-      params.set("query", searchQuery.toLowerCase());
-    }
-    if (subject && subject !== "") {
-      params.set("subject", subject.toLowerCase());
-    }
-    if (university && university !== "") {
-      params.set("university", university.toLowerCase());
-    }
-    router.push(`/search?${params.toString()}`);
-    setSearchQuery("");
+    const handleSearch = () => {
+      const params = new URLSearchParams();
+      if (searchQuery.trim()) {
+        params.set("query", searchQuery.toLowerCase());
+      }
+      if (subject && subject !== "") {
+        params.set("subject", subject.toLowerCase());
+      }
+      if (university && university !== "") {
+        params.set("university", university.toLowerCase());
+      }
+      router.push(`/search?${params.toString()}`);
+      setSearchQuery("");
 
-    // Call the parent-provided onSearch callback
-    onSearch(searchQuery, subject, university);
-  };
+      // Call the parent-provided onSearch callback
+      onSearch(searchQuery, subject, university);
+    };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      handleSearch();
-    }
-  };
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === "Enter") {
+        handleSearch();
+      }
+    };
 
-  const setSelectedSubject = (value: { id: string; name: string }) => {
-    setSubject(value.name);
-    if (isInstant) handleInstantSearch(searchQuery, value.name, university);
-  };
+    const setSelectedSubject = (value: { id: string; name: string }) => {
+      setSubject(value.name);
+      if (isInstant) handleInstantSearch(searchQuery, value.name, university);
+    };
 
-  const setSelectedUniversity = (value: { id: string; name: string }) => {
-    setUniversity(value.name);
-    if (isInstant) handleInstantSearch(searchQuery, subject, value.name);
-  };
+    const setSelectedUniversity = (value: { id: string; name: string }) => {
+      setUniversity(value.name);
+      if (isInstant) handleInstantSearch(searchQuery, subject, value.name);
+    };
 
-  return (
-    <>
-      <Input
-        type="search"
-        placeholder="Wyszukaj prowadzącego zajęcia"
-        value={isInstant ? undefined : searchQuery}
-        onChange={(e) => {
-          setSelectedQuery(e.target.value);
-        }}
-        onKeyDown={isInstant ? undefined : handleKeyDown}
-      />
-      {false /* TODO: v1.1 - Implement filtering, change "false" to isFull */ ? (
-        <>
-          <Combobox data="subjects" title="przedmiot" onChange={setSelectedSubject} />
-          <Combobox data="universities" title="uczelnie" onChange={setSelectedUniversity} />
-        </>
-      ) : null}
-    </>
-  );
-};
+    // Expose the handleSearch function to the parent via ref
+    useImperativeHandle(ref, () => ({
+      handleSearch,
+    }));
+
+    return (
+      <>
+        <Input
+          type="search"
+          placeholder="Wyszukaj prowadzącego zajęcia"
+          value={isInstant ? undefined : searchQuery}
+          onChange={(e) => {
+            setSelectedQuery(e.target.value);
+          }}
+          onKeyDown={isInstant ? undefined : handleKeyDown}
+        />
+        {false /* TODO: v1.1 - Implement filtering, change "false" to isFull */ ? (
+          <>
+            <Combobox data="subjects" title="przedmiot" onChange={setSelectedSubject} />
+            <Combobox data="universities" title="uczelnie" onChange={setSelectedUniversity} />
+          </>
+        ) : null}
+      </>
+    );
+  }
+);
+
+Search.displayName = "Search";
 
 export default Search;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,24 +2,27 @@
 
 import { Loader2, SearchIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Suspense } from "react";
 import Search from "@/components/Search";
 
 const SearchBar = ({ isInstant }: { isInstant: boolean }) => {
   const [isLoading, setIsLoading] = useState(false);
 
+  // Ref to access the handleSearch function from the Search component
+  const searchRef = useRef<{ handleSearch: () => void } | null>(null);
+
   const handleSearch = (query: string, subject: string, university: string) => {
     setIsLoading(true);
     console.log("Search triggered with:", { query, subject, university });
     // Perform additional logic here if needed
-    setTimeout(() => setIsLoading(false), 1000); // Simulate loading
+    setTimeout(() => setIsLoading(false), 5000); // Simulate loading
   };
 
   return (
     <div className="inline-flex gap-3 w-full max-w-lg">
       <Suspense fallback={<div>Loading...</div>}>
-        <Search isInstant={isInstant} onSearch={handleSearch} />
+        <Search ref={searchRef} isInstant={isInstant} onSearch={handleSearch} />
       </Suspense>
       {isInstant ? null : isLoading ? (
         <Button type="button" disabled>
@@ -27,7 +30,7 @@ const SearchBar = ({ isInstant }: { isInstant: boolean }) => {
           <span className="hidden sm:block">Szukaj</span>
         </Button>
       ) : (
-        <Button type="button" onClick={() => handleSearch("", "", "")}>
+        <Button type="button" onClick={() => searchRef.current?.handleSearch()}>
           <SearchIcon />
           <span className="hidden sm:block">Szukaj</span>
         </Button>
